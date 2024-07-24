965c21d0550e4d0c97a6140ff97e3e0587330d8b
Ziji <Zijipia@gmail.com>
2024-07-24 12:04:34 +0700

    edited player button
    edited player button

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = {
     type: "events",
     /**
      * 
-     * @param {Client} client 
+     * @param { Client } client 
      */
     execute: async (client) => {
         if (config.deploy) {
