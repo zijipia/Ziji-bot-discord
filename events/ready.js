47dcd2465b7ea2cea3c19925e9b374da1c8f6c00
Ziji <104454302+zijipia@users.noreply.github.com>
2024-08-03 00:39:14 +0700

    Revert "bat dau"

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,6 @@
 const { Events, Client } = require("discord.js");
 const config = require("../config");
 const deploy = require("../deploy");
-const mongoose = require('mongoose');
 
 module.exports = {
     name: Events.ClientReady,
@@ -14,15 +13,6 @@ module.exports = {
         if (config.deploy) {
             await deploy(client)
         }
-        if (process.env.MONGO) {
-            await mongoose.connect(process.env.MONGO)
-                .then(() => {
-                    console.log('Connected!');
-                    client.db = require("./../utility/mongoDB");
-                }
-                );
-        }
-
         console.log(`Ready! Logged in as ${client.user.tag}`);
         client.user.setActivity({ name: config?.ActivityName || "ziji", type: 2 });
         client.user.setStatus(config?.Status || "online");
