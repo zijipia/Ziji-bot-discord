78d031aa462d17c28d3588036fb0c1ce100109d4
Ziji <Zijipia@gmail.com>
2024-08-29 22:39:17 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,56 +1,55 @@
 const { Events, CommandInteraction } = require("discord.js");
-const config = require("./../config");
-
+const config = require("./../config")
 module.exports = {
     name: Events.InteractionCreate,
-    type: "events",
+    type: "events"
+}
 
-    /**
-     * @param {CommandInteraction} interaction
-     */
-    execute: async (interaction) => {
-        const { client, user } = interaction;
+/**
+ * @param { CommandInteraction } interaction
+ */
+module.exports.execute = async (interaction) => {
+    let command;
+    let commandType;
+    const { client, user } = interaction
+    //cooldowns 
+    if (interaction.isChatInputCommand() || interaction.isAutocomplete() || interaction.isMessageContextMenuCommand()) {
         const now = Date.now();
-        let command, commandType;
-
-        if (interaction.isChatInputCommand() || interaction.isMessageContextMenuCommand() || interaction.isAutocomplete()) {
-            const cooldownAmount = (config?.defaultCooldownDuration ?? 3) * 1000;
+        if (!!client.cooldowns.has(user.id)) {
+            const cooldownAmount = (config?.defaultCooldownDuration ?? 3) * 1_000;
             const expirationTime = client.cooldowns.get(user.id) + cooldownAmount;
-
-            if (client.cooldowns.has(user.id) && now < expirationTime) {
-                return interaction.reply({
-                    content: `Please wait, you are on a cooldown for \`${interaction.commandName}\`. You can use it again <t:${Math.round(expirationTime / 1000)}:R>.`,
-                    ephemeral: true
-                });
+            if (now < expirationTime) {
+                const expiredTimestamp = Math.round(expirationTime / 1_000);
+                return interaction.reply({ content: `Please wait, you are on a cooldown for \`${interaction?.commandName}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
             }
-
-            client.cooldowns.set(user.id, now);
-            command = client.commands.get(interaction.commandName);
-            commandType = 'command';
-        } else if (interaction.isMessageComponent() || interaction.isModalSubmit()) {
-            command = client.functions.get(interaction.customId);
-            commandType = 'function';
         }
+        client.cooldowns.set(user.id, now);
+        command = client.commands.get(interaction.commandName);
+        commandType = 'command';
+    } else if (interaction.isMessageComponent() || interaction.isModalSubmit()) {
+        command = client.functions.get(interaction.isAutocomplete() ? interaction.commandName : interaction.customId);
+        commandType = 'function';
+    }
 
     if (!command) {
         console.error(`No ${commandType} matching ${interaction.commandName || interaction.customId} was found.`);
         return;
     }
 
-        try {
-            if (interaction.isAutocomplete()) {
-                await command.autocomplete(interaction);
-            } else {
-                await command.execute(interaction);
-            }
-        } catch (error) {
-            console.error(error);
-            const response = { content: 'There was an error while executing this command!', ephemeral: true };
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp(response);
-            } else {
-                await interaction.reply(response);
-            }
+    try {
+        if (interaction.isAutocomplete()) {
+            await command.autocomplete(interaction);
+        } else {
+            await command.execute(interaction);
+
+        }
+    } catch (error) {
+        console.error(error);
+        const response = { content: 'There was an error while executing this command!', ephemeral: true };
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp(response);
+        } else {
+            await interaction.reply(response);
         }
     }
 };
