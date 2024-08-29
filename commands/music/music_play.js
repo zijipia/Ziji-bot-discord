99a7fce999b20a5eab9235d4cf93b52fccb91757
Ziji <Zijipia@gmail.com>
2024-08-29 22:54:30 +0700

    autocomplete

--- commands/music/music_play.js
+++ commands/music/music_play.js
@@ -1,8 +1,6 @@
 const { useMainPlayer, useQueue } = require("discord-player");
-const { CommandInteraction } = require("discord.js");
+const { CommandInteraction, AutocompleteInteraction } = require("discord.js");
 const player = useMainPlayer()
-
-
 
 module.exports.data = {
     name: "play",
@@ -61,4 +59,32 @@ module.exports.execute = async (interaction) => {
     } else {
         await command.execute(interaction, query);
     }
-}
+    return;
+};
+/**
+ * 
+ * @param { AutocompleteInteraction } interaction 
+ */
+module.exports.autocomplete = async (interaction) => {
+    try {
+        const query = interaction.options.getString('query', true);
+        if (!query) return;
+
+        const results = await player.search(query, {
+            fallbackSearchEngine: "youtube",
+        });
+
+        const tracks = results.tracks
+            .filter(t => t.title.length > 0 && t.title.length < 100 && t.url.length > 0 && t.url.length < 100)
+            .slice(0, 10);
+
+        if (!tracks.length) return;
+
+        await interaction.respond(
+            tracks.map(t => ({ name: t.title, value: t.url }))
+        );
+        return;
+    } catch (e) {
+        console.error(e);
+    }
+};
