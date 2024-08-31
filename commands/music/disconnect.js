dcbd498153394111b9e3d50137353b9bd41e9ba4
Ayakaa <tuadtt.k56F1@student.foe.edu.vn>
2024-08-31 18:21:24 +0700

    up

--- commands/music/disconnect.js
+++ commands/music/disconnect.js
@@ -1,32 +1,34 @@
-const { useMainPlayer, useQueue } = require("discord-player");
-const { CommandInteraction } = require("discord.js");
-const player = useMainPlayer()
-
-
+const { useMainPlayer, useQueue } = require('discord-player');
+const { CommandInteraction } = require('discord.js');
+const player = useMainPlayer();
 
 module.exports.data = {
-    name: "disconnect",
-    description: "Tắt nhạc và rời khỏi kênh thoại",
-    type: 1, // slash commad
-    options: [],
-    integration_types: [0],
-    contexts: [0],
-}
+  name: 'disconnect',
+  description: 'Tắt nhạc và rời khỏi kênh thoại',
+  type: 1, // slash commad
+  options: [],
+  integration_types: [0],
+  contexts: [0],
+  global: true,
+};
 /**
- * 
- * @param { CommandInteraction } interaction 
+ *
+ * @param { CommandInteraction } interaction
  */
 module.exports.execute = async (interaction, lang) => {
-    await interaction.deferReply();
-    const queue = useQueue(interaction.guild.id);
-    if (!queue) {
-        await interaction?.guild?.members?.me?.voice?.disconnect();
-        await interaction.editReply("Đã ngắt kết nói");
-        return
-    }
-    if (queue.metadata.LockStatus && queue.metadata.requestedBy?.id !== interaction.user?.id) return;
-    await queue?.metadata?.mess?.edit({ components: [] }).catch(e => { })
-    queue.delete();
-    await interaction.editReply("Đã tắt nhạc");
-
-}
+  await interaction.deferReply();
+  const queue = useQueue(interaction.guild.id);
+  if (!queue) {
+    await interaction?.guild?.members?.me?.voice?.disconnect();
+    await interaction.editReply('Đã ngắt kết nói');
+    return;
+  }
+  if (
+    queue.metadata.LockStatus &&
+    queue.metadata.requestedBy?.id !== interaction.user?.id
+  )
+    return;
+  await queue?.metadata?.mess?.edit({ components: [] }).catch((e) => {});
+  queue.delete();
+  await interaction.editReply('Đã tắt nhạc');
+};
--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    deploy: true,
-    ActivityName: "Ziji bot",
-    Status: "idle",
-    defaultCooldownDuration: 5,
-    ImageSearch: true,
-    DeafultLang: "vi",
-}
+  deploy: true,
+  ActivityName: '/help',
+  Status: 'online',
+  defaultCooldownDuration: 5,
+  ImageSearch: true,
+  DeafultLang: 'vi',
+  OwnerID: 'Your ID',
+  DevGuild: 'Your Guild ID',
+};
