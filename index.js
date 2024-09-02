59e5a8875bf85423bed265f5a49d3452048d5f03
Ayakaa <tuadtt.k56F1@student.foe.edu.vn>
2024-09-02 19:32:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -138,6 +138,7 @@ const initialize = async () => {
     loadEvents(path.join(__dirname, 'events'), client),
     loadEvents(path.join(__dirname, 'discord-player'), player.events),
   ]);
+  dfgdfg;
 
   client.login(process.env.TOKEN);
 };
