a2fa6c06e1f2b34beef89b044e2d97d8f32342be
Ayakaa <tuadtt.k56F1@student.foe.edu.vn>
2024-09-03 11:20:34 +0700

    chore: update config.js with default settings

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ module.exports = {
   // Cho phép deploy command bot
   deploy: true,
   // Tên hoạt động của bot
-  ActivityName: 'Ziji bot',
+  ActivityName: '/help',
   // Trạng thái mặc định của bot 'online', 'idle', 'dnd', 'invisible'
-  Status: 'idle',
+  Status: 'online',
   // Thời gian chờ mặc định giữa các lệnh (tính bằng giây)
   defaultCooldownDuration: 5,
   // Bật tính năng tìm kiếm hình ảnh 'true', 'false' nên tắt khi chạy trên server vì khá tốn tài nguyên
   ImageSearch: true,
   // Ngôn ngữ mặc định của bot (vi, en, ...)
   DeafultLang: 'vi',
-  // ID của chủ sở hữu bot (người dùng có thể thực hiện lệnh owner) 'ID admin'
-  OwnerID: '891275176409460746',
+  // Danh sách ID của chủ sở hữu bot (người dùng có thể thực hiện lệnh owner) ["ID admin", "ID admin", ...]
+  OwnerID: [''],
   // Danh sách ID của các server dành cho nhà phát triển ["ID server", "ID server", ...] or []
-  DevGuild: [],
+  DevGuild: [''],
 };
