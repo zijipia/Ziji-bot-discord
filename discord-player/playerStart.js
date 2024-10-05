a9d8290ce53123ff079f56138e1e15dfc4d5f6ed
Ziji <Zijipia@gmail.com>
2024-10-05 14:32:44 +0700

    Add syncedLyrics to playerStart

--- discord-player/playerStart.js
+++ discord-player/playerStart.js
@@ -1,6 +1,10 @@
-const { useMainPlayer, GuildQueue, Track, GuildQueueEvent } = require('discord-player');
+const { GuildQueue, Track, GuildQueueEvent } = require('discord-player');
 const config = require('../config');
-const players = useMainPlayer();
+
+async function SendNewMessenger(queue, playerGui) {
+  queue.metadata.mess = await queue.metadata.channel.send(playerGui);
+  return;
+}
 
 module.exports = {
   name: GuildQueueEvent.playerStart,
@@ -12,13 +16,27 @@ module.exports = {
    * @returns
    */
   execute: async (queue, track) => {
-    const player = players.client.functions.get('player_func');
+    const { client } = queue.player;
 
-    if (!player) return;
-    const res = await player.execute(players.client, queue, track);
-    if (queue.metadata.mess) return queue.metadata.mess.edit(res);
-    const EditMetadata = players.client.functions.get('EditMetadata');
-    const messengerr = await queue.metadata.channel.send(res);
-    await EditMetadata.execute(queue.guild, { mess: messengerr });
+    const player_func = client.functions.get('player_func');
+    if (!player_func) return;
+
+    const playerGui = await player_func.execute(client, queue, track);
+
+    // send messenger
+    if (!queue.metadata.mess) {
+      await SendNewMessenger(queue, playerGui);
+      return;
+    }
+    // edit messenger
+    await queue.metadata.mess.edit(playerGui).catch(async () => await SendNewMessenger(queue, playerGui));
+    // lyrics
+    const ZiLyrics = queue.metadata.ZiLyrics;
+    if (ZiLyrics.Active) {
+      const Lyrics = client.functions.get('Lyrics');
+      if (!Lyrics) return;
+      await Lyrics.execute(null, { type: 'syncedLyrics', queue });
+      return;
+    }
   },
 };
