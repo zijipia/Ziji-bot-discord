727663737351d2c0453d71743d5f2de61373c7d5
Ziji <Zijipia@gmail.com>
2024-10-08 21:42:34 +0700

    add to button

--- functions/button/B_player_refresh.js
+++ functions/button/B_player_refresh.js
@@ -13,7 +13,7 @@ module.exports.data = {
  */
 
 module.exports.execute = async ({ interaction, lang }) => {
-	interaction.deferUpdate();
+	await interaction.deferUpdate();
 	const queue = useQueue(interaction.guild.id);
 	if (!queue) return;
 	const player = interaction.client.functions.get("player_func");
