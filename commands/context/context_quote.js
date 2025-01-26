5a1f4d34adf52a365b8beb9022087454686904ab
Akimiya <105062613+dunghoangdacminh@users.noreply.github.com>
2025-01-26 16:10:57 +0000

    quote

---
+++ commands/context/context_quote.js
@@ -0,0 +1,31 @@
+const { MiQ } = require("makeitaquote");
+
+module.exports.data = {
+	name: "Quote Image Generation",
+	type: 3, // context
+	options: [],
+	integration_types: [0],
+	contexts: [0],
+};
+/**
+ * @param { object } context - object command
+ * @param { import ("discord.js").MessageContextMenuCommandInteraction } context.interaction - interaction
+ * @param { import('../../lang/vi.js') } context.lang - language
+ */
+module.exports.execute = async ({ interaction, lang }) => {
+    await interaction.deferReply();
+    let msg = interaction.targetMessage;
+
+    if (msg.content) {
+        const miq = new MiQ()
+            .setText(msg.content)
+            .setColor(true)
+            .setDisplayname(msg.author.displayName)
+            .setUsername(msg.author.username)
+            .setAvatar(msg.author.displayAvatarURL())
+            .setWatermark(interaction.client.user.tag);
+        const result = await miq.generate();
+        await interaction.editReply(result);
+        return
+    }
+}
