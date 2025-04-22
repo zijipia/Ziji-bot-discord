122df754c6c5cff380f8c2e4cc203468a31b8953
Akimiya <105062613+dunghoangdacminh@users.noreply.github.com>
2025-04-22 09:17:46 +0000

    some useful functions

---
+++ functions/utils/successEmbed.js
@@ -0,0 +1,16 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports.data = {
+    name: "createSuccessEmbed",
+    type: "utils",
+};
+
+module.exports.execute = async (message) => {
+    const embed = new EmbedBuilder()
+        .setTitle(`✅ | Thành công`)
+        .setDescription(message)
+        .setColor('Green')
+        .setTimestamp()
+        .setThumbnail(require('@zibot/zihooks').useClient().user.displayAvatarURL({ size: 1024 }));
+    return embed;
+}
