3de067ede114e118c539e58d05a0db9e3b5a6806
Akimiya <105062613+dunghoangdacminh@users.noreply.github.com>
2025-05-30 13:43:26 +0000

    confessions

--- functions/utils/successEmbed.js
+++ functions/utils/successEmbed.js
@@ -5,12 +5,12 @@ module.exports.data = {
 	type: "utils",
 };
 
-module.exports.execute = async (message) => {
+module.exports.execute = (message) => {
 	const embed = new EmbedBuilder()
 		.setTitle(`✅ | Thành công`)
 		.setDescription(message)
 		.setColor("Green")
 		.setTimestamp()
 		.setThumbnail(require("@zibot/zihooks").useClient().user.displayAvatarURL({ size: 1024 }));
-	return embed;
+	return embed.data;
 };
